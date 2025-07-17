45221eaab3fca1f3b27c3eb05184d4ebf8d7c98e
RobTou <92668877+RobTou@users.noreply.github.com>
2025-07-17 09:56:45 +0200

    Create index.js

---
+++ backend-apis/lab_exercise-tracker/index.js
@@ -0,0 +1,84 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+require('dotenv').config()
+
+app.use(cors())
+app.use(express.static('public'))
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html')
+});
+
+// Middleware to parse incoming request bodies
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+let users = [];
+let exercises = [];
+let userIdCounter = 1;
+
+// POST route to create a new user.
+app.post('/api/users', (req, res) => {
+  const username = req.body.username;
+  const newUser = {
+    username,
+    _id: userIdCounter++
+  };
+  users.push(newUser);
+  res.json(newUser);
+});
+
+// Retrieve all users
+app.get('/api/users', (req, res) => {
+  res.json(users);
+});
+
+// Route to log an exercise for a user
+app.post('/api/users/:_id/exercises', (req, res) => {
+  const userId = req.params._id;
+  const { description, duration, date } = req.body;
+  const user = users.find((user) => user._id = userId);
+
+  if (!user) {
+    return res.status(404).send('User not found');
+  }
+
+  const exerciseDate = date ? new Date(date): new Date();
+  const newExercise = {
+    username: user.username,
+    description,
+    duration,
+    date: exerciseDate.toDateString
+  };
+
+  exercises.push({userId, ...newExercise});
+  res.json({ ...user, ...newExercise });
+});
+
+// Route to retrieve a user's exercise log
+app.get('/api/users/:_id/log', (req, res) => {
+  const userId = req.params._id;
+  const userExercises = exercises.filter(ex => ex.userId === userId);
+  const user = users.find((user) => user._id = userId);
+
+  if (!user) {
+    return res.status(404).send('User not found');
+  }
+
+  const log = userExercises.map(ex => ({
+    description: ex.description,
+    duration: ex.duration,
+    date: ex.date
+  }));
+
+  res.json({
+    username: user.username,
+    count: log.length,
+    _id: userId,
+    log
+  });
+});
+
+const listener = app.listen(process.env.PORT || 3000, () => {
+  console.log('Your app is listening on port ' + listener.address().port)
+})
